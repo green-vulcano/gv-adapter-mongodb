6986fa49719ab8f58d7370b9e5aa5ea4ac9d0b5a
Michele Reale <mrambler147@gmail.com>
2018-07-31 16:26:31 +0200

    MongoDBCallOperationTest test suite: insertOne() and insertMany() tests implemented; renaming a field used in tests in order to avoid confusion with the init() data

--- src/test/java/it/greenvulcano/gvesb/virtual/mongodb/MongoDBCallOperationTest.java
+++ src/test/java/it/greenvulcano/gvesb/virtual/mongodb/MongoDBCallOperationTest.java
@@ -104,6 +104,91 @@ public class MongoDBCallOperationTest {
 	}
 
 	@Test
+	public void testInsertOne() throws GVException {
+
+		// CREATE a single BATTERY record
+
+		GVBuffer inputCreateGVBuffer = new GVBuffer();
+		inputCreateGVBuffer.setService("TEST");
+		inputCreateGVBuffer.setObject("{ \"fakePhysicalId\": \"BATTERY\" }");
+
+		GreenVulcano greenVulcano = new GreenVulcano();
+		GVBuffer outputCreateGVBuffer = greenVulcano.forward(inputCreateGVBuffer, "testCreate");
+
+		assertNotNull(outputCreateGVBuffer.getObject());
+
+		JSONObject createResult = new JSONObject(outputCreateGVBuffer.getObject().toString());
+
+		String insertedDocumentId = null;
+
+		try {
+
+			insertedDocumentId = createResult.getJSONObject("_id").getString("$oid");
+
+		} catch (Exception e) { throw new GVException(e.getMessage()); }
+
+
+
+		// READ the inserted record
+
+		GVBuffer inputFindGVBuffer = new GVBuffer();
+		inputFindGVBuffer.setService("TEST");
+		inputFindGVBuffer.setProperty("FILTER", "{\"_id\": { $oid:\"" + insertedDocumentId + "\" } }");
+		inputFindGVBuffer.setObject("test input");
+
+		GVBuffer outputFindGVBuffer = greenVulcano.forward(inputFindGVBuffer, "testFind");
+
+		assertNotNull(outputFindGVBuffer.getObject());
+
+		JSONArray find1Result = new JSONArray(outputFindGVBuffer.getObject().toString());
+		assertEquals(1, find1Result.length());
+		assertTrue(IntStream.range(0, find1Result.length())
+				.mapToObj(find1Result::getJSONObject)
+				.map(battery-> battery.getJSONObject("_id").getString("$oid"))
+				.allMatch(insertedDocumentId::equals));
+
+	}
+
+	@Test
+	public void testInsertMany() throws GVException {
+
+		// CREATE a BATTERY record and a GPS record
+
+		GVBuffer inputCreateGVBuffer = new GVBuffer();
+		inputCreateGVBuffer.setService("TEST");
+		inputCreateGVBuffer.setObject("[{ \"fakePhysicalId\": \"BATTERY\" }, { \"fakePhysicalId\": \"GPS\" }]");
+
+		GreenVulcano greenVulcano = new GreenVulcano();
+		GVBuffer outputCreateGVBuffer = greenVulcano.forward(inputCreateGVBuffer, "testCreate");
+
+		assertNotNull(outputCreateGVBuffer.getObject());
+
+		JSONArray createResult = new JSONArray(outputCreateGVBuffer.getObject().toString());
+
+		assertEquals(2, createResult.length());
+
+
+
+		// READ the inserted records
+
+		GVBuffer inputFindGVBuffer = new GVBuffer();
+		inputFindGVBuffer.setService("TEST");
+		inputFindGVBuffer.setProperty("FILTER", "{ \"$or\": [ { \"fakePhysicalId\": \"BATTERY\" }, { \"fakePhysicalId\": \"GPS\" } ] }");
+		inputFindGVBuffer.setObject("test input");
+
+		GVBuffer outputFindGVBuffer = greenVulcano.forward(inputFindGVBuffer, "testFind");
+
+		assertNotNull(outputFindGVBuffer.getObject());
+
+		JSONArray find1Result = new JSONArray(outputFindGVBuffer.getObject().toString());
+		assertEquals(2, find1Result.length());
+		assertTrue(IntStream.range(0, find1Result.length())
+				.mapToObj(find1Result::getJSONObject)
+				.map(device-> device.getString("fakePhysicalId")).allMatch(id -> id.equals("BATTERY") || id.equals("GPS")));
+
+	}
+
+	@Test
 	public void testUpdate() throws GVException {
 
 		// Update all BATTERY records adding new field
@@ -144,7 +229,7 @@ public class MongoDBCallOperationTest {
 
 		GVBuffer inputCreateGVBuffer = new GVBuffer();
 		inputCreateGVBuffer.setService("TEST");
-		inputCreateGVBuffer.setObject("{ \"physicalId\": \"BATTERY\" }");
+		inputCreateGVBuffer.setObject("{ \"fakePhysicalId\": \"BATTERY\" }");
 
 		GreenVulcano greenVulcano = new GreenVulcano();
 		GVBuffer outputCreateGVBuffer = greenVulcano.forward(inputCreateGVBuffer, "testCreate");
@@ -219,11 +304,13 @@ public class MongoDBCallOperationTest {
 	@Test
 	public void testDeleteMany() throws GVException {
 
+
+
 		// CREATE a BATTERY record and a GPS record
 
 		GVBuffer inputCreateGVBuffer = new GVBuffer();
 		inputCreateGVBuffer.setService("TEST");
-		inputCreateGVBuffer.setObject("[{ \"physicalId\": \"BATTERY\" }, { \"physicalId\": \"GPS\" }]");
+		inputCreateGVBuffer.setObject("[{ \"fakePhysicalId\": \"BATTERY\" }, { \"fakePhysicalId\": \"GPS\" }]");
 
 		GreenVulcano greenVulcano = new GreenVulcano();
 		GVBuffer outputCreateGVBuffer = greenVulcano.forward(inputCreateGVBuffer, "testCreate");
@@ -240,7 +327,7 @@ public class MongoDBCallOperationTest {
 
 		GVBuffer inputFind1GVBuffer = new GVBuffer();
 		inputFind1GVBuffer.setService("TEST");
-		inputFind1GVBuffer.setProperty("FILTER", "{ \"$or\": [ { \"physicalId\": \"BATTERY\" }, { \"physicalId\": \"GPS\" } ] }");
+		inputFind1GVBuffer.setProperty("FILTER", "{ \"$or\": [ { \"fakePhysicalId\": \"BATTERY\" }, { \"fakePhysicalId\": \"GPS\" } ] }");
 		inputFind1GVBuffer.setObject("test input");
 
 		GVBuffer outputFind1GVBuffer = greenVulcano.forward(inputFind1GVBuffer, "testFind");
@@ -251,7 +338,7 @@ public class MongoDBCallOperationTest {
 		assertEquals(2, find1Result.length());
 		assertTrue(IntStream.range(0, find1Result.length())
 				.mapToObj(find1Result::getJSONObject)
-				.map(device-> device.getString("physicalId")).allMatch(id -> id.equals("BATTERY") || id.equals("GPS")));
+				.map(device-> device.getString("fakePhysicalId")).allMatch(id -> id.equals("BATTERY") || id.equals("GPS")));
 
 
 
@@ -259,7 +346,7 @@ public class MongoDBCallOperationTest {
 
 		GVBuffer inputDeleteGVBuffer = new GVBuffer();
 		inputDeleteGVBuffer.setService("TEST");
-		inputDeleteGVBuffer.setObject("{ \"$or\": [ { \"physicalId\": \"BATTERY\" }, { \"physicalId\": \"GPS\" } ] }");
+		inputDeleteGVBuffer.setObject("{ \"$or\": [ { \"fakePhysicalId\": \"BATTERY\" }, { \"fakePhysicalId\": \"GPS\" } ] }");
 
 		GVBuffer outputDeleteGVBuffer = greenVulcano.forward(inputDeleteGVBuffer, "testDelete");
 
@@ -276,7 +363,7 @@ public class MongoDBCallOperationTest {
 
 		GVBuffer inputFind2GVBuffer = new GVBuffer();
 		inputFind2GVBuffer.setService("TEST");
-		inputFind2GVBuffer.setProperty("FILTER", "{ \"$or\": [ { \"physicalId\": \"BATTERY\" }, { \"physicalId\": \"GPS\" } ] }");
+		inputFind2GVBuffer.setProperty("FILTER", "{ \"$or\": [ { \"fakePhysicalId\": \"BATTERY\" }, { \"fakePhysicalId\": \"GPS\" } ] }");
 		inputFind2GVBuffer.setObject("test input");
 
 		GVBuffer outputFind2GVBuffer = greenVulcano.forward(inputFind2GVBuffer, "testFind");
